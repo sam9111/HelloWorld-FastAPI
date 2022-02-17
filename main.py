695e513e422f5d4bd0e222cba283da82e08cb2b9
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-02-17 19:43:19 +0530

    emotion processing

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-import asyncio
 import json
-import os
-import time
-from datetime import datetime
 
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from newscatcherapi import NewsCatcherApiClient
 
-import utils
-
-load_dotenv()
+from utils import *
 
 app = FastAPI()
 
@@ -27,54 +19,11 @@ app.add_middleware(
 )
 
 
-newscatcherapi = NewsCatcherApiClient(
-    x_api_key=os.getenv("API_KEY")
-)  # use 10,000 calls account in production
-
-
-def make_data(response):
-    articles = response["articles"]
-    news = {"countries": {}, "last_fetched": ""}
-    news_by_country = news["countries"]
-    for article in articles:
-        if article["country"] == "unknown":
-            continue
-        article_obj = {
-            "id": article["_id"],
-            "title": article["title"],
-            "summary": article["summary"],
-            "topics": article["topic"],
-            "published_date": article["published_date"],
-            "link": article["link"],
-            "country": article["country"],
-            "media": article["media"],
-        }
-
-        if (country := article["country"]) not in news_by_country:
-            country_obj = {"articles": [article_obj], "total": 1}
-            news_by_country[country] = country_obj
-        else:
-            news_by_country[country]["articles"].append(article_obj)
-            news_by_country[country]["total"] += 1
-    news["last_fetched"] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    return news
-
-
-def update_data():
-    response = newscatcherapi.get_latest_headlines_all_pages(
-        lang="en", when="24h", max_page=1  # remove in production
-    )
-
-    with open("news.json", "w") as outfile:
-        json.dump(make_data(response), outfile)
-
-
-def get_data():
-    with open("news.json", "r") as infile:
-        data = json.load(infile)
-    return data
+@app.get("/news")  # unprocessed news
+def news():
+    return get_news()
 
 
 @app.get("/")
-def news():
+def data():
     return get_data()
---
+++ utils.py
@@ -0,0 +1,102 @@
+import json
+import os
+from datetime import datetime
+import re
+
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from newscatcherapi import NewsCatcherApiClient
+import text2emotion as te
+from pprint import pprint
+
+load_dotenv()
+
+
+newscatcherapi = NewsCatcherApiClient(
+    x_api_key=os.getenv("API_KEY")
+)  # use 10,000 calls account in production
+
+
+def make_news(response):
+    articles = response["articles"]
+    news = {"countries": {}, "last_fetched": ""}
+    news_by_country = news["countries"]
+    for article in articles:
+        if article["country"] == "unknown":
+            continue
+        article_obj = {
+            "title": article["title"],
+            "summary": article["summary"],
+            "topics": article["topic"],
+            "published_date": article["published_date"],
+            "link": article["link"],
+            "country": article["country"],
+            "media": article["media"],
+        }
+
+        if (country := article["country"]) not in news_by_country:
+            country_obj = {"articles": {}, "total": 1}
+            country_obj["articles"][article["_id"]] = article_obj
+            news_by_country[country] = country_obj
+        else:
+            news_by_country[country]["articles"][article["_id"]] = article_obj
+            news_by_country[country]["total"] += 1
+    news["last_fetched"] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    return news
+
+
+def update_news():
+    response = newscatcherapi.get_latest_headlines_all_pages(
+        lang="en", when="24h", max_page=1  # remove in production
+    )
+    with open("news.json", "w") as outfile:
+        json.dump(make_news(response), outfile)
+
+
+def get_news():
+    with open("news.json", "r") as infile:
+        news_json = json.load(infile)
+    return news_json
+
+
+def process_text(text):
+    emotion_obj = te.get_emotion(text)
+    emotion_list = sorted(emotion_obj.items(), key=lambda item: item[1], reverse=True)
+    if all(value == 0 for value in emotion_list):
+        return False
+    return list(emotion_list[0])
+
+
+def make_data():
+    news_json = get_news()
+    news_by_country = news_json["countries"]
+    to_be_deleted = []
+    new_obj = {"countries": {}}
+    for country in news_by_country:
+
+        articles = news_by_country[country]["articles"]
+        emotions = []
+        for article_id in articles:
+            article_obj = articles[article_id]
+            text = article_obj["title"]
+            emotion_list = process_text(text)
+            if not emotion_list:
+                continue
+            emotion_list.append(article_id)
+            emotions.append(emotion_list)
+        emotions.sort(key=lambda x: x[1], reverse=True)
+        articles[emotions[0][2]]["top_emotion"] = emotions[0][0]
+        new_obj["countries"][country] = {"article": {}}
+        new_obj["countries"][country]["article"] = articles[emotions[0][2]]
+    return new_obj
+
+
+def update_data():
+    with open("data.json", "w") as outfile:
+        json.dump(make_data(), outfile)
+
+
+def get_data():
+    with open("data.json", "r") as infile:
+        data_json = json.load(infile)
+    return data_json
