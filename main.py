1ae796bf41138565660a512dac8d8460e468c4dc
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-02-25 20:02:34 +0530

    remove allow credentials

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ origins = ["http://localhost:3000", "localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
